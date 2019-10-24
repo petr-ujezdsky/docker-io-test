eaa8be296b8a20c267acd4a5e5f3bacebd36eb13
Petr Újezdský <petr.ujezdsky@gmail.com>
2019-10-24 22:13:16 +0200

    Better results formating for google spreadsheet, added test repetition option

--- README.md
+++ README.md
@@ -2,9 +2,52 @@
 Simple IO performance test for docker. Relates to my [issue on GitHub](https://github.com/docker/for-mac/issues/3771).
 
 ## How to start it
-Simply run
+For 3 repeated runs for current number of assigned cores run
 ```
-$ ./test.sh
+$ ./test.sh 3
+34,69	0,34	11,55	1,98	1,40
+29,73	0,32	7,43	1,77	0,98
+33,73	0,28	7,27	1,60	1,24
+```
+
+To test it natively run
+```
+$ INSIDE=true ./test.sh 3
+0,64	0,02	0,13	0,21	0,09
+0,69	0,02	0,11	0,17	0,12
+0,73	0,01	0,11	0,16	0,09
+```
+
+You can also add `--verbose` to print test names
+```
+$ ./test.sh 3 --verbose
+
+TEST - write 100 MiB file
+100000+0 records in
+100000+0 records out
+real 27.33
+user 0.43
+sys 3.28
+
+TEST - read 100 MiB file
+real 0.31
+user 0.00
+sys 0.06
+
+TEST - copy 100 MiB file
+real 8.69
+user 0.00
+sys 1.22
+
+TEST - write 1000 small files
+real 2.25
+user 0.04
+sys 0.20
+
+TEST - delete all tmp files
+real 1.75
+user 0.01
+sys 0.16
 ```
 
 ## Results
@@ -13,6 +56,6 @@ Here are results for my machine (MacBook Pro 15-inch, 2017).
 > `CPU count = 0` means native run in MacOS.
 
 Raw data available at [my google sheet](https://docs.google.com/spreadsheets/d/11Kijkn0wK83d_zOvmTW9A77vbMC1fbG8LwLygLysqkU/edit?usp=sharing).
- 
+
 Docker Desktop version 2.0.0.3 (31259) / stable / 8858db33c8
 ![](result.png)
--- test.sh
+++ test.sh
@@ -1,31 +1,48 @@
 #!/bin/sh
 
 if [[ "$INSIDE" != "true" ]]; then
-  docker run --rm -it -e INSIDE=true -v "$(pwd)/:/test/" --workdir /test alpine ./test.sh
+  docker run --rm -it -e INSIDE=true -v "$(pwd)/:/test/" --workdir /test alpine ./test.sh $@
   exit
 fi
 
-mkdir tmp
+parse() {
+  while IFS= read -r line
+  do
+    if [ "$1" == "--verbose" ]; then
+      echo $line
+    else
+      if echo "$line" | grep "real" > /dev/null; then
+        echo "$line" | sed -e 's/real //' -e 's/\./,/' -e 's/\\n//' | tr '\n' '\t'
+      fi
+    fi
+  done < "/dev/stdin"
+}
 
-echo
-echo "TEST - write 100 MiB file"
-time dd if=/dev/zero of=./tmp/test.dat bs=1024 count=100000
-echo
+for i in $(seq $1)
+do
+  mkdir tmp 2> /dev/null
 
-echo "TEST - read 100 MiB file"
-time cat ./tmp/test.dat > /dev/null
-echo
+  (
+    echo
+    echo "TEST - write 100 MiB file"
+    time -p dd if=/dev/zero of=./tmp/test.dat bs=1024 count=100000
+    echo
 
-echo "TEST - copy 100 MiB file"
-time cp ./tmp/test.dat ./tmp/test2.dat
-echo
+    echo "TEST - read 100 MiB file"
+    time -p cat ./tmp/test.dat > /dev/null
+    echo
 
-echo "TEST - write 1000 small files"
-# manyFiles
-# time { for i in $(seq 10); do echo "I am very small";  done }
-time ./manyFiles.sh
-echo
+    echo "TEST - copy 100 MiB file"
+    time -p cp ./tmp/test.dat ./tmp/test2.dat
+    echo
 
-echo "TEST - delete all tmp files"
-time rm -rf ./tmp
-echo
+    echo "TEST - write 1000 small files"
+    time -p ./manyFiles.sh
+    echo
+
+    echo "TEST - delete all tmp files"
+    time -p rm -rf ./tmp
+    echo
+  ) 2>&1 | parse $2
+  echo
+done
